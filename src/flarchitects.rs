8436cfe8d6c851840e8724d2733730a9d5a7b20c
Zachary Spar <zachspar@gmail.com>
2019-09-02 16:53:12 -0700

    update template flarchitects

--- src/flarchitects.rs
+++ src/flarchitects.rs
@@ -14,11 +14,28 @@ pub fn view_template_txt(app_name: &str, view_name: &str) -> String {
     return String::from(ret_string);
 }
 
-pub fn init_template_txt(app_name: &str) -> String {
-    let ret_string = format!("import flask\napp = flask.Flask(__name__)\
-                             \napp.config.from_object('{}.config')\
-                             \nimport {}\nimport {}.views\nimport {}.config\n",
-                             app_name, app_name, app_name, app_name);
+
+pub fn init_template_txt(app_name: &str, tmp_type: &str) -> String {
+
+    let mut ret_string: String = "".to_string();
+    if tmp_type == "root" {
+        ret_string = String::from(format!("import flask\napp = flask.Flask(__name__)\
+                                  \nimport {}\nimport {}.views\n", app_name, app_name));
+    }
+    else if tmp_type == "view" {
+        ret_string = String::from(format!("from {}.views import *\n", app_name));
+    }
+
+    assert!(ret_string != "");
+    return ret_string;
+}
+
+
+pub fn run_app_script_txt(app_name: &str, cwd: &String) -> String {
+    let ret_string = format!("#!/bin/bash\nsource {}/env/bin/activate;\
+                             export LC_ALL=en_US.UTF-8;export LANG=en_US.UTF-8;\
+                             export FLASK_DEBUG=True;export FLASK_APP={}/{};flask run",
+                             cwd, cwd, app_name);
     return String::from(ret_string);
 }
 
